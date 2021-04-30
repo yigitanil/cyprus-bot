1159f53ab812dc199e28e5736450d12b30611318
Yigit <yigitan@gmail.com>
2021-04-30 16:20:37 +0300

    changes

--- src/main/kotlin/com/ygt/cyprusbot/service/TelegramClientService.kt
+++ src/main/kotlin/com/ygt/cyprusbot/service/TelegramClientService.kt
@@ -9,25 +9,28 @@ import org.springframework.web.util.UriBuilder
 import reactor.util.retry.Retry
 
 @Service
-class TelegramClientService( @Value("\${telegram.token}") val token: String,
-                             @Value("\${telegram.chatId}") val chatId: String,
-                             @Qualifier("telegramWebClient") val telegramWebClient: WebClient) {
+class TelegramClientService(
+    @Value("\${telegram.token}") val token: String,
+    @Value("\${telegram.chatId}") val chatId: String,
+    @Qualifier("telegramWebClient") val telegramWebClient: WebClient
+) {
 
     fun sendMessageAsync(message: String?) {
         sendMessage(message)
-                .retryWhen(Retry.withThrowable { it.all { t -> t::class.java == WebClientRequestException::class.java } })
-                .subscribe()
+            .retryWhen(Retry.withThrowable { it.all { t -> t::class.java == WebClientRequestException::class.java } })
+            .subscribe()
     }
 
     fun sendMessage(message: String?) = telegramWebClient
-            .get()
-            .uri { uriBuilder: UriBuilder ->
-                uriBuilder.pathSegment(token, "sendMessage")
-                        .queryParam("chat_id", chatId)
-                        .queryParam("text", message)
-                        .queryParam("parse_mode", "markdown")
-                        .build()
-            }.retrieve()
-            .bodyToMono(String::class.java)
-            .doOnNext { s: String? -> println("Message is sent to chat. Message: $message") }
+        .get()
+        .uri { uriBuilder: UriBuilder ->
+            uriBuilder.pathSegment(token, "sendMessage")
+                .queryParam("chat_id", chatId)
+                .queryParam("text", message)
+                .queryParam("parse_mode", "markdown")
+                .build()
+        }.retrieve()
+        .bodyToMono(String::class.java)
+        .doOnNext { s: String? -> println("Message is sent to chat. Message: $message") }
+        .doOnError { it.printStackTrace() }
 }
--- src/main/kotlin/com/ygt/cyprusbot/service/scheduler/TrackerScheduler.kt
+++ src/main/kotlin/com/ygt/cyprusbot/service/scheduler/TrackerScheduler.kt
@@ -4,7 +4,9 @@ import com.binance.api.client.domain.market.CandlestickInterval
 import com.ygt.cyprusbot.model.Strategies
 import com.ygt.cyprusbot.service.BinanceClientService
 import com.ygt.cyprusbot.service.TelegramClientService
-import com.ygt.cyprusbot.strategy.*
+import com.ygt.cyprusbot.strategy.CustomStrategy
+import com.ygt.cyprusbot.strategy.MavilimWStrategy
+import com.ygt.cyprusbot.strategy.TilsonT3Strategy
 import mu.KotlinLogging
 import org.apache.commons.lang3.StringUtils
 import org.springframework.scheduling.annotation.Scheduled
@@ -21,8 +23,8 @@ class TrackerScheduler(
 
 
     @Scheduled(cron = "02 0 */1 * * *")
-    fun runFuture() {
-        log.info { "Future tracker is started" }
+    fun runMavilim() {
+        log.info { "MavilimW tracker is started" }
 
         binanceClientService
             .getFutureExchangeInfo()
@@ -30,9 +32,31 @@ class TrackerScheduler(
             .filter { it.contractType.equals("PERPETUAL") }
             .parallel()
             .flatMap { binanceClientService.getFutureCandlesticks(it.symbol, CandlestickInterval.HOURLY.intervalId) }
-            .map { runStrategy(it, Strategies.MAVILIMW, "FUTURE") }
+            .map { runStrategy(it, Strategies.MAVILIMW) }
             .filter(StringUtils::isNotEmpty)
-            .reduce { s1, s2 -> "- " + s1 + " \n " + s2 }
+            .reduce { s1, s2 -> s1 + " \n" + s2 }
+            .map { "__MAVILIMW - 1H__\n" + it }
+            .doOnNext { println(it) }
+            .doOnNext { telegramClientService.sendMessageAsync(it) }
+            .subscribe()
+
+
+    }
+
+    @Scheduled(cron = "03 0 */1 * * *")
+    fun runTilsonT3() {
+        log.info { "Tilson T3 tracker is started" }
+
+        binanceClientService
+            .getFutureExchangeInfo()
+            .flatMapMany { Flux.fromIterable(it.symbols) }
+            .filter { it.contractType.equals("PERPETUAL") }
+            .parallel()
+            .flatMap { binanceClientService.getFutureCandlesticks(it.symbol, CandlestickInterval.HOURLY.intervalId) }
+            .map { runStrategy(it, Strategies.TILSON_T3) }
+            .filter(StringUtils::isNotEmpty)
+            .reduce { s1, s2 -> s1 + " \n" + s2 }
+            .map { "__TILSONT3 - 1H__\n" + it }
             .doOnNext { println(it) }
             .doOnNext { telegramClientService.sendMessageAsync(it) }
             .subscribe()
@@ -41,50 +65,32 @@ class TrackerScheduler(
     }
 
 
-    private fun runStrategy(it: BaseBarSeries, strategyType: Strategies, market: String): String {
-        if (strategyType == Strategies.STOCH) {
-            val strategy = InverseFisherTransformStochStrategy(it, 21, 9)
-            return runStrategy(it, strategyType, strategy, market)
-        }
-        if (strategyType == Strategies.TILSONT3_MAVILIM) {
-            val strategy = TilsonT3MavilimStrategy(it)
-            return runStrategy(it, strategyType, strategy, market)
-        }
-        if (strategyType == Strategies.COMBO_1H) {
-            val strategy = Combo1HStrategy(it)
-            return runStrategy(it, strategyType, strategy, market)
-        }
-        if (strategyType == Strategies.LARGE_PIN) {
-            val strategy = LargePinStrategy(it)
-            return runStrategy(it, strategyType, strategy, market)
-        }
-        if (strategyType == Strategies.MACD_DEMA) {
-            val strategy = MacdStrategy(it)
-            return runStrategy(it, strategyType, strategy, market)
-        }
+    private fun runStrategy(it: BaseBarSeries, strategyType: Strategies): String {
         if (strategyType == Strategies.MAVILIMW) {
             val strategy = MavilimWStrategy(it)
-            return runStrategy(it, strategyType, strategy, market)
+            return runStrategy(it, strategy)
+        }
+        if (strategyType == Strategies.TILSON_T3) {
+            val strategy = TilsonT3Strategy(it, 0.5, 3)
+            return runStrategy(it, strategy)
         }
         return "";
     }
 
     private fun runStrategy(
         it: BaseBarSeries,
-        strategyType: Strategies,
-        strategy: CustomStrategy,
-        market: String
+        strategy: CustomStrategy
     ): String {
         val ndx = it.getBarCount() - 1
         val evaluate = strategy.evaluate(ndx)
-        val prefix = "*${it.name.toUpperCase()}, $strategyType ${it.lastBar.timePeriod}"
+        val prefix = "*$${it.name.toUpperCase().replace("_PERP", "")}*, ${it.lastBar.closePrice}"
         if (evaluate == 1) {
             log.info { "$prefix ,Entry point  ${it.lastBar}" }
-            return "$prefix, ${strategyType.enterMessage}, Last price: ${it.lastBar.closePrice}";
+            return "$prefix, **BUY**";
         }
         if (evaluate == -1) {
             log.info { "$prefix ,Exit point  ${it.lastBar}" }
-            return "$prefix, ${strategyType.exitMessage}, Last price: ${it.lastBar.closePrice}";
+            return "$prefix, *SELL*";
         }
         return "";
     }
